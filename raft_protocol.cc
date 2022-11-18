25368cd54130a8dce72614130812fe869f0eaade
ZhaoHaoRu <zhaohaoru@sjtu.edu.cn>
2022-11-18 20:54:42 +0800

    update lab3

--- raft_protocol.cc
+++ raft_protocol.cc
@@ -2,7 +2,15 @@
 
 marshall &operator<<(marshall &m, const request_vote_args &args) {
     // Lab3: Your code here
-    
+    // TODO: I am confused about the header
+    // add the request header
+    req_header new_req_header;
+    m.pack_req_header(new_req_header);
+    // add the content
+    m = m << args.term;
+    m = m << args.candidate_id;
+    m = m << args.last_log_index;
+    m = m << args.last_log_term;
     return m;
 }
 unmarshall &operator>>(unmarshall &u, request_vote_args &args) {
